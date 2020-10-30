fa2f323d3083afc68ac097d169e4ea860c8b5228
joaopaulo11jp <joaopaulo11jp@gmail.com>
2020-10-30 20:50:04 -0300

    Openapi header

---
+++ src/main/java/io/github/joaopaulo11jp/school/SchoolApplication.java
@@ -0,0 +1,30 @@
+package io.github.joaopaulo11jp.school;
+
+import javax.ws.rs.core.Application;
+
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@OpenAPIDefinition(
+	    tags = {
+	            @Tag(name="teachers", description="Teachers"),
+	            @Tag(name="students", description="Students")
+	    },
+	    info = @Info(
+	        title="School API",
+	        version = "1.0.0-SNAPSHOT",
+	        contact = @Contact(
+	            name = "João Paulo",
+	            url = "http://joaopaulo.github.io",
+	            email = "joaopaulo11jp@gmail.com"),
+	        license = @License(
+	            name = "Apache 2.0",
+	            url = "http://www.apache.org/licenses/LICENSE-2.0.html"))
+	)
+public class SchoolApplication extends Application {
+//There's no functional need to use the JAX-RS Application class here on quarkus
+//but for documenting reasons to follow the openapi microprofile's specification
+}
